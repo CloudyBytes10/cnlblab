b094893ae32aa88add082543eefe5161c23fd93b
CloudBytes <cloudybytes@cloudybytes>
2024-01-10 17:35:04 +0900

    Initial commit

---
+++ nodes/master.sh
@@ -0,0 +1,12 @@
+export MASTER_IP=$(ip a |grep global | grep -v '10.0.2.15' | grep '192.168.80' | awk '{print $2}' | cut -f1 -d '/')
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik --disable servicelb --disable-cloud-controller --kube-proxy-arg proxy-mode=ipvs --flannel-iface=eth1 \
+--node-ip=${MASTER_IP} --node-external-ip=${MASTER_IP} \
+--bind-address=${MASTER_IP}" sh -
+
+echo $MASTER_IP > /vagrant/master-ip
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/k3s.yaml
+sudo sed -i -e "s/127.0.0.1/${MASTER_IP}/g" /vagrant/k3s.yaml
+/vagrant/k8s-scripts/cluster_readiness.sh
+sysctl net.core.netdev_max_backlog=10000
